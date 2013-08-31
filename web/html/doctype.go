090b565b6cfebb5e3c9fb2b464ad4760b73f5cbc
Vincent Letourneau <vincent@nanoninja.com>
2013-08-31 14:17:37 +0200

    Add doctype

--- web/html/doctype.go
+++ web/html/doctype.go
@@ -4,6 +4,8 @@
 
 package html
 
+var doctype map[Doctype]string
+
 type Doctype string
 
 const (
@@ -22,3 +24,50 @@ const (
     MATH_ML20       Doctype = "MathML 2.0"
     MATH_ML_101     Doctype = "MathML 1.01"
 )
+
+func init() {
+    doctype = make(map[Doctype]string)
+
+    eol := "\n"
+    tab := "\t"
+
+    // HTML 5
+    doctype[HTML5] = `<!DOCTYPE HTML>` + eol
+
+    // HTML 4.01 Strict
+    doctype[HTML401_STRICT] = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"` + eol
+    doctype[HTML401_STRICT] += tab + `"http://www.w3.org/TR/html4/strict.dtd">` + eol
+
+    // HTML 4.01 Transitional
+    doctype[HTML401_TRANS] = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"` + eol
+    doctype[HTML401_TRANS] += tab + `"http://www.w3.org/TR/html4/loose.dtd">` + eol
+
+    // HTML 4.01 Frameset
+    doctype[HTML401_FRAME] = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"` + eol
+    doctype[HTML401_FRAME] += tab + `"http://www.w3.org/TR/html4/frameset.dtd">` + eol
+
+    // XHTML 1.0 Strict
+    doctype[XHTML_10_STRICT] = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"` + eol
+    doctype[XHTML_10_STRICT] += tab + `"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">` + eol
+
+    // XHTML 1.0 Transitional
+    doctype[XHTML_10_TANS] = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"` + eol
+    doctype[XHTML_10_TANS] += tab + `"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">` + eol
+
+    // XHTML 1.0 Frameset
+    doctype[XHTML_10_FRAME] = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"` + eol
+    doctype[XHTML_10_FRAME] += tab + `"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">` + eol
+
+    // XHTML 1.1 DTD
+    doctype[XHTML_11] = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"` + eol
+    doctype[XHTML_11] += tab + `"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">` + eol
+
+    // XHTML Basic 1.1
+    doctype[XHTML_BASIC_11] = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"` + eol
+    doctype[XHTML_BASIC_11] += `"http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">` + eol
+}
+
+// Returns the doctype by name
+func GetDoctype(name Doctype) string {
+    return doctype[name]
+}
